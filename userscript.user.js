ca33ee5ef8477fb5eac69b7ae9a0ffb4ea38d211
personalgeria <102892566+personalgeria@users.noreply.github.com>
2022-04-03 10:06:23 +0200

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Morocco
+// @name         FUMAR
 // @namespace    h
 // @version      0.1
 // @description  try to take over the canvas!
@@ -13,8 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-//            i.src = "https://i.imgur.com/rPWfrJo.png";
-            i.src = "https://i.imgur.com/g8yAIzl.png"
+            i.src = "https://github.com/personalgeria/place/blob/main/template_na.png?raw=true"
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
             console.log(i);
             return i;
